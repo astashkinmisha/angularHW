0783496715108b85e3e8f1e2abacd47a74ff2a75
misha <trickerforyou@gmail.com>
2021-01-19 13:59:41 +0200

    angular1hw

---
+++ src/app/cars/cars.ts
@@ -0,0 +1,14 @@
+export const cars = [
+  {name: 'bmw', volume: 3, maxSpeed: 300},
+  {name: 'opel', volume: 2, maxSpeed: 250},
+  {name: 'mersedes', volume: 4, maxSpeed: 323},
+  {name: 'bentley', volume: 4, maxSpeed: 400},
+  {name: 'volvo', volume: 4, maxSpeed: 542},
+  {name: 'lexux', volume: 4, maxSpeed: 444},
+  {name: 'aston martin', volume: 4, maxSpeed: 543},
+  {name: 'vaxhuall', volume: 4, maxSpeed: 324},
+  {name: 'honda', volume: 4, maxSpeed: 234},
+  {name: 'kia', volume: 4, maxSpeed: 325},
+  {name: 'dodge', volume: 4, maxSpeed: 566}
+];
+
