edd08567a32650c3e58d3a1cceddd68e143a2364
misha <trickerforyou@gmail.com>
2021-01-19 13:59:41 +0200

    angular1hw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarssComponent } from './carss/carss.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CarsComponent,
+    CarssComponent
   ],
   imports: [
     BrowserModule
