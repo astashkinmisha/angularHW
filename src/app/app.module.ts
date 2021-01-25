8a8dc9ec5ad0deb4cf499dcb96d383188620eab7
misha <trickerforyou@gmail.com>
2021-01-25 17:41:10 +0200

    angular3HW

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import {RouterModule} from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { FullCarComponent } from './components/full-car/full-car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    HomeComponent,
+    CarsComponent,
+    FullCarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{
+      path: 'link/home', component: HomeComponent
+    },
+      {
+      path: 'link/cars', component: CarsComponent, children: [
+          {path: ':id', component: FullCarComponent}
+        ]
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/data/cars.ts
@@ -0,0 +1,13 @@
+export const cars = [
+  {name: 'bmw', volume: 3, maxSpeed: 300, status: false},
+  {name: 'opel', volume: 2, maxSpeed: 250, status: true},
+  {name: 'mersedes', volume: 4, maxSpeed: 323, status: true},
+  {name: 'bentley', volume: 4, maxSpeed: 400, status: false},
+  {name: 'volvo', volume: 4, maxSpeed: 542, status: false},
+  {name: 'lexux', volume: 4, maxSpeed: 444, status: true},
+  {name: 'aston martin', volume: 4, maxSpeed: 543, status: false},
+  {name: 'vaxhuall', volume: 4, maxSpeed: 324, status: true},
+  {name: 'honda', volume: 4, maxSpeed: 234, status: true},
+  {name: 'kia', volume: 4, maxSpeed: 325, status: true},
+  {name: 'dodge', volume: 4, maxSpeed: 566, status: false}
+];
